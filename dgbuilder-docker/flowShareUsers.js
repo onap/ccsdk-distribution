b7d0c7d286fc254ae43f0ceec9f1873c6fdee0ac
Chinthakayala, Sheshashailavas (sc2914) <sc2914@att.com>
2017-08-28 09:00:36 -0900

    [CCSDK-29]changes for dgbuilder docker image
    changes related to creating docker
    container for dgbuilder
    
    Change-Id: Ida8710be20c6737c46f7f01bc36e0fe11dd250bb
    Issue-ID:{CCSDK-29}
    Signed-off-by: Chinthakayala, Sheshashailavas (sc2914) <sc2914@att.com>

---
+++ dgbuilder-docker/flowShareUsers.js
@@ -0,0 +1,8 @@
+module.exports = {"flowShareUsers":
+	[
+	{
+          "name" : "Release 1702",
+          "rootDir" : "1702"
+	}
+	]
+}
---
+++ dgbuilder-docker/releases/1702/customSettings.js
@@ -0,0 +1,20 @@
+module.exports = {
+		'name' : 'Release 1702',
+		'emailAddress' :'change_email_id@dgbuilder.com',
+		'uiPort' :3100,
+		'mqttReconnectTime': 15000,
+		'serialReconnectTime' : 15000,
+		'debugMaxLength': 1000,
+		'htmlPath': 'releases/1702/html/',
+		'xmlPath': 'releases/1702/xml/',
+		'flowFile' : 'releases/1702/flows/flows.json',
+		'sharedDir': 'releases/1702/flows/shared',
+		'userDir' : 'releases/1702',
+		'httpAuth': {user:'dguser',pass:'cc03e747a6afbbcbf8be7668acfebee5'},
+		'dbHost': 'dbhost',
+		'dbPort': '3306',
+		'dbName': 'sdnctl',
+		'dbUser': 'sdnctl',
+		'dbPassword': 'gamma',
+		'gitLocalRepository': ''
+		}
