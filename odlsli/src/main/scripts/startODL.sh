912f98b9cdfe0f636239b8f94fccf7858bffac26
Dan Timoney <dtimoney@att.com>
2018-02-27 14:10:22 -0500

    Create nitrogen dockers
    Updated dockers to install Nitrogen artifacts.
    
    Change-Id: I9710346f064fa5fff0d92b5d6de32cc84d9e1b3d
    Issue-ID: CCSDK-179
    Signed-off-by: Dan Timoney <dtimoney@att.com>

--- odlsli/src/main/scripts/startODL.sh
+++ odlsli/src/main/scripts/startODL.sh
@@ -26,7 +26,7 @@
 
 ODL_HOME=${ODL_HOME:-/opt/opendaylight/current}
 ODL_ADMIN_PASSWORD=${ODL_ADMIN_PASSWORD:-Kp8bJ4SXszM0WXlhak3eHlcse2gAw84vaoGGmJvUy2U}
-SDNC_HOME=${SDNC_HOME:-/opt/onap/sdnc}
+CCSDK_HOME=${CCSDK_HOME:-/opt/onap/ccsdk}
 SLEEP_TIME=${SLEEP_TIME:-120}
 MYSQL_PASSWD=${MYSQL_PASSWD:-openECOMP1.0}
 
@@ -41,26 +41,26 @@ do
 done
 echo -e "\nmysql ready"
 
-if [ ! -f ${SDNC_HOME}/.installed ]
+if [ ! -f ${CCSDK_HOME}/.installed ]
 then
 	echo "Installing SDN-C database"
-	${SDNC_HOME}/bin/installSdncDb.sh
+	${CCSDK_HOME}/bin/installSdncDb.sh
 	echo "Starting OpenDaylight"
 	${ODL_HOME}/bin/start
 	echo "Waiting ${SLEEP_TIME} seconds for OpenDaylight to initialize"
 	sleep ${SLEEP_TIME}
 	echo "Installing SDN-C platform features"
-	${SDNC_HOME}/bin/installFeatures.sh
-	if [ -x ${SDNC_HOME}/svclogic/bin/install.sh ]
+	${CCSDK_HOME}/bin/installFeatures.sh
+	if [ -x ${CCSDK_HOME}/svclogic/bin/install.sh ]
 	then
 		echo "Installing directed graphs"
-		${SDNC_HOME}/svclogic/bin/install.sh
+		${CCSDK_HOME}/svclogic/bin/install.sh
 	fi
 
 
 	echo "Restarting OpenDaylight"
 	${ODL_HOME}/bin/stop
-	echo "Installed at `date`" > ${SDNC_HOME}/.installed
+	echo "Installed at `date`" > ${CCSDK_HOME}/.installed
 fi
 
 exec ${ODL_HOME}/bin/karaf
