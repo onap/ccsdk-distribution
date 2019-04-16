1013c67b39364a0d7d65e1cdccbe715e95fcec74
Timoney, Dan (dt5972) <dtimoney@att.com>
2019-04-16 10:37:48 -0400

    Revert "Backport Dublin ansible server"
    This reverts commit 6f7a88678334fc0d42c219e72ec5c76d8eb9bb75 and
    instead does a more focused change, to move the apt gets and pip
    installs from the startup script to the Dockerfile.
    
    Issue-ID: CCSDK-1166
    Change-Id: I2e1db5459a22559a13a604fb3122b3abbce216c0
    Signed-off-by: Timoney, Dan (dt5972) <dtimoney@att.com>

--- ansible-server/src/main/scripts/startAnsibleServer.sh
+++ ansible-server/src/main/scripts/startAnsibleServer.sh
@@ -3,16 +3,6 @@ exec &> >(tee -a "/var/log/ansible-server.log")
 
 if [ ! -f /tmp/.ansible-server-installed ]
 then
-    pip install 'more-itertools==5.0.0'
-    pip install PyMySQL
-    pip install 'cherrypy<18.0.0'
-    pip install requests
-
-    apt-get update
-    apt-get -y install software-properties-common
-    apt-add-repository -y ppa:ansible/ansible
-    apt-get -y install ansible
-
     cp /etc/ansible/ansible.cfg /etc/ansible/ansible.cfg.orig
     cat /etc/ansible/ansible.cfg.orig | sed -e 's/#host_key_checking/host_key_checking/' > /etc/ansible/ansible.cfg
     date > /tmp/.ansible-server-installed 2>&1
