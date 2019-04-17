bbe7f58af19c7e07d4dbf67f072ca835ebf3dd84
Timoney, Dan (dt5972) <dtimoney@att.com>
2019-04-17 14:26:04 -0400

    Disable staging plugins
    Disable staging plugin to avoid interfering with staging Jenkins job
    
    Change-Id: If35283e981a8862047d67c7e586240ed80974061
    Issue-ID: CCSDK-1166
    Signed-off-by: Timoney, Dan (dt5972) <dtimoney@att.com>

--- ansible-server/src/main/scripts/startAnsibleServer.sh
+++ ansible-server/src/main/scripts/startAnsibleServer.sh
@@ -3,8 +3,20 @@ exec &> >(tee -a "/var/log/ansible-server.log")
 
 if [ ! -f /tmp/.ansible-server-installed ]
 then
-    cp /etc/ansible/ansible.cfg /etc/ansible/ansible.cfg.orig
-    cat /etc/ansible/ansible.cfg.orig | sed -e 's/#host_key_checking/host_key_checking/' > /etc/ansible/ansible.cfg
+	if [ -f /etc/ansible/ansible.cfg ]
+	then
+		cp /etc/ansible/ansible.cfg /etc/ansible/ansible.cfg.orig
+        cat /etc/ansible/ansible.cfg.orig | sed -e 's/#host_key_checking/host_key_checking/' > /etc/ansible/ansible.cfg
+    else
+    	if [ ! -d /etc/ansible ]
+    	then
+    		mkdir /etc/ansible
+        fi
+        cat > /etc/ansible/ansible.cfg <<-END
+[defaults]
+host_key_checking = False
+END
+    fi
     date > /tmp/.ansible-server-installed 2>&1
 fi
 
