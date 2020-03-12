ca10eb08e791b9771e1e54ccfa69a623019d2dc9
Dan Timoney <dtimoney@att.com>
2020-03-12 08:19:12 -0400

    Fix licenses on ODL scripts
    ODL scripts need to be dual licensed (EPL 1.0 and Apache 2.0).
    
    Change-Id: I3dfa85144c168f9dddd1fb9966776488f527b9be
    Issue-ID: CCSDK-2172
    Signed-off-by: Dan Timoney <dtimoney@att.com>

--- opendaylight/neon/neon-alpine/src/main/odlscripts/configure_cluster.sh
+++ opendaylight/neon/neon-alpine/src/main/odlscripts/configure_cluster.sh
@@ -4,7 +4,11 @@
 #
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Public License v1.0 which accompanies this distribution,
-# and is available at http://www.eclipse.org/legal/epl-v10.html
+# and is available at http://www.eclipse.org/legal/epl-v10.html , or the Apache License,
+# Version 2.0 which is available at https://www.apache.org/licenses/LICENSE-2.0
+#
+# SPDX-License-Identifier: EPL-1.0 OR Apache-2.0
+#
 #
 
 
