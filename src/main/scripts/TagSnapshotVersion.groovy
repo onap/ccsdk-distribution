40cbe0200546aff38eb33745266f4becc7d3ebe4
Lasse Kaihlavirta <l.kaihlavirt@partner.samsung.com>
2020-12-03 17:54:13 +0200

    Move CSIT to ccsdk/distribution repo
     - move CCSDK healthcheck CSIT under distribution repository
     - add run-project-csit.sh that takes common CSIT scripts from
       integration/csit on runtime and then executes all the plans
       under distribution/csit/plans
     - use version.properties and UNIQUE_DOCKER_TAG to identify the
       docker image versions to run for testing if the latter is given,
       otherwise just choose "latest" (to enable easy local testing)
     - introduce new groovy script for tagging SNAPSHOT images only
       based on version.properties and UNIQUE_DOCKER_TAG
     - apply UNIQUE_DOCKER_TAG in dgbuilder and odlsli-alpine docker
       image versioning
     - use docker-compose for teardown instead of common kill scripts
    
    Issue-ID: CCSDK-3017
    Signed-off-by: Lasse Kaihlavirta <l.kaihlavirt@partner.samsung.com>
    Change-Id: I37dcf682bf1ab77016a76adc89f5658bb0b71b19

---
+++ src/main/scripts/TagSnapshotVersion.groovy
@@ -0,0 +1,34 @@
+/*-
+ * ============LICENSE_START=======================================================
+ * ONAP CCSDK
+ * ================================================================================
+ * Copyright (C) 2020 Samsung Electronics Co., Ltd.
+ * ================================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============LICENSE_END============================================
+ * ===================================================================
+ *
+ */
+
+package org.onap.ccsdk.distribution
+
+Properties properties = new Properties()
+File propertiesFile = new File(new File(getClass().protectionDomain.codeSource.location.path).parent + '/../../../version.properties')
+propertiesFile.withInputStream {
+    properties.load(it)
+}
+
+project.properties['project.docker.latestminortag.version']=properties.release_name + '.' + properties.sprint_number + "-SNAPSHOT-latest";
+project.properties['project.docker.latestfulltag.version']=properties.release_name + '.' + properties.sprint_number + '.' + properties.feature_revision + "-SNAPSHOT-latest";
+project.properties['project.docker.latesttagtimestamp.version']=properties.release_name + '.' + properties.sprint_number + '.' + properties.feature_revision + "-SNAPSHOT-"+project.properties['ccsdk.build.timestamp'];
+project.properties['project.docker.uniquedockertag.version']=properties.release_name + '.' + properties.sprint_number + '.' + properties.feature_revision + "-SNAPSHOT-"+project.properties['uniquedockertag'];
