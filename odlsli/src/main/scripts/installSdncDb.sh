195cd6548a0f2a1ac02aa6943e569171ec1adc04
Singal, Kapil (ks220y) <ks220y@att.com>
2021-02-18 12:52:50 -0500

    Enhacing ODL-SLI startODL.sh and installSdncDb.sh
    Issue-ID: CCSDK-3173
    Signed-off-by: Singal, Kapil (ks220y) <ks220y@att.com>
    Change-Id: I5054b086170e3cc6cf7d714d144c4170459d1c7e

--- odlsli/src/main/scripts/installSdncDb.sh
+++ odlsli/src/main/scripts/installSdncDb.sh
@@ -22,18 +22,29 @@
 ###
 
 CCSDK_HOME=${CCSDK_HOME:-/opt/onap/ccsdk}
+MYSQL_HOST=${MYSQL_HOST:-dbhost}
 MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-openECOMP1.0}
 
 SDNC_DB_USER=${SDNC_DB_USER:-sdnctl}
 SDNC_DB_PASSWD=${SDNC_DB_PASSWD:-gamma}
 SDNC_DB_DATABASE=${SDN_DB_DATABASE:-sdnctl}
 
+#
+# Wait for database
+#
+echo "Waiting for mysql: ${MYSQL_HOST}"
+until mysql -h ${MYSQL_HOST} -u root -p${MYSQL_ROOT_PASSWORD} mysql &> /dev/null
+do
+  printf "."
+  sleep 1
+done
 
+echo -e "\nCreating $SDNC_DB_DATABASE and user $SDNC_DB_USER"
 # Create tablespace and user account
-mysql -h dbhost -u root -p${MYSQL_ROOT_PASSWORD} mysql <<-END
-CREATE DATABASE ${SDNC_DB_DATABASE};
-CREATE USER '${SDNC_DB_USER}'@'localhost' IDENTIFIED BY '${SDNC_DB_PASSWD}';
-CREATE USER '${SDNC_DB_USER}'@'%' IDENTIFIED BY '${SDNC_DB_PASSWD}';
+mysql -h ${MYSQL_HOST} -u root -p${MYSQL_ROOT_PASSWORD} mysql <<-END
+CREATE DATABASE IF NOT EXISTS ${SDNC_DB_DATABASE} ;
+CREATE USER IF NOT EXISTS '${SDNC_DB_USER}'@'localhost' IDENTIFIED BY '${SDNC_DB_PASSWD}';
+CREATE USER IF NOT EXISTS '${SDNC_DB_USER}'@'%' IDENTIFIED BY '${SDNC_DB_PASSWD}';
 GRANT ALL PRIVILEGES ON ${SDNC_DB_DATABASE}.* TO '${SDNC_DB_USER}'@'localhost' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON ${SDNC_DB_DATABASE}.* TO '${SDNC_DB_USER}'@'%' WITH GRANT OPTION;
 commit;
@@ -41,5 +52,7 @@ END
 
 if [ -f ${CCSDK_HOME}/data/odlsli.dump ]
 then
-mysql -h dbhost -u root -p${MYSQL_ROOT_PASSWD} sdnctl < ${CCSDK_HOME}/data/odlsli.dump
+    mysql -h ${MYSQL_HOST} -u root -p${MYSQL_ROOT_PASSWD} sdnctl < ${CCSDK_HOME}/data/odlsli.dump
 fi
+
+echo -e "\nmysql: ${MYSQL_HOST} is ready."
