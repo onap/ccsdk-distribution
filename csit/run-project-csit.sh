1860bfa149f88c0a99bcd70d46f5717e7bf945c8
Lasse Kaihlavirta <l.kaihlavirt@partner.samsung.com>
2021-02-18 16:12:46 +0200

    Simplify CCSDK Distribution CSIT
     - remove runtime dependency to integation/csit and copy the scripts
       to local repo
     - remove unnecessary chrome and selenium installations
     - remove obsolete comment reference to UNIQUE_DOCKER_TAG
    
    Issue-ID: CCSDK-3174
    Signed-off-by: Lasse Kaihlavirta <l.kaihlavirt@partner.samsung.com>
    Change-Id: Iaedb8f853ca048817b403c1e0e0c67563a65db8a

--- csit/run-project-csit.sh
+++ csit/run-project-csit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 #
-# Copyright 2020 © Samsung Electronics Co., Ltd.
+# Copyright 2020-2021 © Samsung Electronics Co., Ltd.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,32 +17,14 @@
 
 # $1 test options (passed on to run-csit.sh as such)
 #
-# UNIQUE_DOCKER_TAG environment variable is expected to be used
-# by all test plans whenever it is defined. If it's not defined,
-# local execution is assumed and plain "latest" should be used
-# by setup scripts to allow the use of locally build docker images
-#
 
 export TESTOPTIONS=${1}
-# GERRIT_BRANCH for checking out integration/csit should be the same as
-# current project clone branch
-export GERRIT_BRANCH=${GERRIT_BRANCH:-`git rev-parse --abbrev-ref HEAD`}
 export WORKSPACE=$(git rev-parse --show-toplevel)/csit
 
 rm -rf ${WORKSPACE}/archives
 mkdir -p ${WORKSPACE}/archives
-rm -rf ${WORKSPACE}/data
-mkdir -p ${WORKSPACE}/data
-
-cd ${WORKSPACE}/data
-git clone https://gerrit.onap.org/r/integration/csit
-cd csit
-# make best-effort attempt to checkout branch that corresponds to current
-# project repository clone if the checkout fails for any reason,
-# the cloned integration/csit remains on master
-git checkout ${GERRIT_BRANCH}
-cp *.sh ${WORKSPACE}/
 cd ${WORKSPACE}
+
 # Execute all testsuites defined under plans subdirectory
 for dir in plans/*/
 do
